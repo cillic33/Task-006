cbc32a7c976db7651519a30ab4ab4e0d0fc70e81
Margarita <cillic@yandex.ru>
2021-12-14 13:18:39 +0700

    minimizing js

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const md = 880
     let docWidth
 
+    // на ширине экрана менее md 'menu-content' становится выпадашкой
     setDropoutClass = () => {
         docWidth = document.documentElement.clientWidth
 
@@ -22,19 +23,23 @@ document.addEventListener('DOMContentLoaded', () => {
     })
 
 
-    hideAllDropouts = (param) => {
+    hideAllDropouts = (class_dropout) => {
+        // удаляем активность у всех заголовков, кроме 'menu-bars'
         $('.header__menu-item_dropdown').removeClass('header__menu-item_active')
         $('.header__person_dropdown').removeClass('header__person_active')
         $('.header__catalog-title_dropdown').removeClass('header__catalog-title_active')
 
-        if (param == 'geo') {
+        if (class_dropout == 'header__geo-dropout') {
+            // если закрываем выпадашку 'geo', то закрываем все выпадашки, кроме 'menu-content'
             $('.dropout').not('.header__menu-content').slideUp()
         } else {
+            // иначе - закрываем все выпадашки и удаляем активность у 'menu-bars'
             $('.header__menu-bars_dropdown').removeClass('header__menu-bars_active')
             $('.dropout').slideUp()
         }
     }
 
+    // при клике в любом месте экрана - закрываем все выпадашки
     window.addEventListener('click', e => {
         const target = e.target
         if (!(target).closest('.dropout') && !(target).closest('.header__menu-item_dropdown') && !(target).closest('.header__person_dropdown') && !(target).closest('.header__menu-bars_dropdown') && !(target).closest('.header__catalog-title_dropdown')) {
@@ -42,51 +47,21 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
-    toggleGeoDropout = (el) => {
-        if ($(el).hasClass('header__menu-item_active')) {
-            $(el).removeClass('header__menu-item_active')
-            $('.header__geo-dropout').slideUp()
+    toggleDropout = (el) => {
+        let class_active = $(el).attr('class').split(' ')[0] + '_active'
+        let class_dropout = $(el).attr('data-class-dropout')
+
+        if ($(el).hasClass(class_active)) {
+            $(el).removeClass(class_active)
+            $('.'+class_dropout).slideUp()
         } else {
-            hideAllDropouts('geo')
-            $(el).addClass('header__menu-item_active')
-            $('.header__geo-dropout').slideDown()
+            hideAllDropouts(class_dropout)
+            $(el).addClass(class_active)
+            $('.'+class_dropout).slideDown()
         }
     }
 
-    togglePersonDropout = (el) => {
-        if ($(el).hasClass('header__person_active')) {
-            $(el).removeClass('header__person_active')
-            $('.header__person-dropout').slideUp()
-        } else {
-            hideAllDropouts('')
-            $(el).addClass('header__person_active')
-            $('.header__person-dropout').slideDown()
-        }
-    }
-
-    toggleHeaderMenuDropout = (el) => {
-        if ($(el).hasClass('header__menu-bars_active')) {
-            $(el).removeClass('header__menu-bars_active')
-            $('.header__menu-content').slideUp()
-        } else {
-            hideAllDropouts('')
-            $(el).addClass('header__menu-bars_active')
-            $('.header__menu-content').slideDown()
-        }
-    }
-
-    toggleCatalogDropout = (el) => {
-        if ($(el).hasClass('header__catalog-title_active')) {
-            $(el).removeClass('header__catalog-title_active')
-            $('.header__catalog-content').slideUp()
-        } else {
-            hideAllDropouts('')
-            $(el).addClass('header__catalog-title_active')
-            $('.header__catalog-content').slideDown()
-        }
-    }
-
-
+    // toggle для блока 'products' в блоке 'order-block'
     orderBlockMore = (id) => {
         if ($('#'+id+' .order-block__more').hasClass('order-block__more_active')) {
             $('#'+id+' .order-block__more').removeClass('order-block__more_active')
